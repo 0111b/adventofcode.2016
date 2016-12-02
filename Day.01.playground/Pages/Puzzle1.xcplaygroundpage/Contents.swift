51316a7eeecbe61b5af8bd6f3039e038ff0640d0
Alexandr Goncharov <ag@bekitzur.com>
2016-12-03 02:45:40 +0300

    Move models to the common

--- Day.01.playground/Pages/Puzzle1.xcplaygroundpage/Contents.swift
+++ Day.01.playground/Pages/Puzzle1.xcplaygroundpage/Contents.swift
@@ -25,138 +25,7 @@ import Foundation
  __How many blocks__ away is Easter Bunny HQ?
  */
 
-//: ## Implementation
-
-//: Possible turns
-enum Turn  {
-    case left, right
-}
-
-//: Coordinate
-struct Coordinate {
-    let x: Int
-    let y: Int
-//: Returns a moved coordinate
-    func moved(length: Int, in orientation: Orientation) -> Coordinate {
-        switch orientation {
-        case .north: return Coordinate(x: x, y: y + length)
-        case .south: return Coordinate(x: x, y: y - length)
-        case .west: return Coordinate(x: x - length, y: y)
-        case .east: return Coordinate(x: x + length, y: y)
-        }
-    }
-}
-
-//: Possible orientations
-enum Orientation {
-    case north, south, east, west
-//: Rotates the orientation
-    func rotate(turn: Turn) -> Orientation {
-        switch self {
-        case .north:
-            switch turn {
-            case .left:
-                return .west
-            case .right:
-                return .east
-            }
-        case .south:
-            switch turn {
-            case .left:
-                return .east
-            case .right:
-                return .west
-            }
-        case .east:
-            switch turn {
-            case .left:
-                return .north
-            case .right:
-                return .south
-            }
-        case .west:
-            switch turn {
-            case .left:
-                return .south
-            case .right:
-                return .north
-            }
-        }
-    }
-}
-
-//: Describes the move
-struct Move {
-    let turn: Turn
-    let length: Int
-}
-
-//: Describes the position
-struct Position {
-    let coordniate: Coordinate
-    let orientation: Orientation
-//: Perform actual move
-    func walked(_ action: Move) -> Position {
-        let orientation = self.orientation.rotate(turn: action.turn)
-        let coordinate = self.coordniate.moved(length: action.length, in: orientation)
-        return Position(coordniate: coordinate, orientation: orientation)
-    }
-}
-//: ## Helpers
-
-extension Position: CustomStringConvertible {
-    var description: String {
-        return "\(coordniate) - \(orientation)"
-    }
-}
-
-extension Coordinate: CustomStringConvertible {
-    var description: String {
-        return "(\(x),\(y))"
-    }
-}
-
-extension Position {
-    func walk(_ moves: [Move]) -> Position {
-        return moves.reduce(self) { position, move in
-            return position.walked(move)
-        }
-    }
-}
-
-extension String {
-    func moves() -> [Move] {
-        do {
-            return try components(separatedBy: ", ").map { try Move(raw: $0) }
-        } catch let error {
-            print(error)
-            return []
-        }
-    }
-}
-
-
-extension Move {
-    init(raw: String) throws {
-        guard raw.characters.count >= 2 else { throw ParsingError.invalidLength }
-        let turnString = String(raw[raw.startIndex])
-        let lengthString = raw.substring(from: raw.index(after: raw.startIndex))
-        guard let parsedLength = Int(lengthString)  else  { throw ParsingError.invalidLength }
-        switch turnString {
-            case "L": turn = .left
-            case "R": turn = .right
-            default: throw ParsingError.invalidTurn
-        }
-        length = parsedLength
-    }
-    enum ParsingError: Error {
-        case invalidString
-        case invalidLength
-        case invalidTurn
-    }
-}
-
-
+//: ---
 //: ## Execution
 
 let start = Position(coordniate: Coordinate(x: 0, y: 0), orientation: .north)
@@ -172,9 +41,12 @@ distanceFromZero(coordinate: sample2.coordniate) == 2
 let sample3 = start.walk("R5, L5, R5, R3".moves())
 distanceFromZero(coordinate: sample3.coordniate) == 12
 
+//: ---
+
 let input = "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
 let finalPoint = start.walk(input.moves())
 distanceFromZero(coordinate: finalPoint.coordniate)
 
+//: ---
 //: [Next](@next)
 
---
+++ Day.01.playground/Sources/Common.swift
@@ -0,0 +1,162 @@
+import Foundation
+
+//: ## Implementation
+
+//: Possible turns
+public enum Turn  {
+    case left, right
+}
+
+//: Coordinate
+public struct Coordinate {
+    public let x: Int
+    public let y: Int
+    
+    public init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    //: Returns a moved coordinate
+    public func moved(length: Int, in orientation: Orientation) -> Coordinate {
+        switch orientation {
+        case .north: return Coordinate(x: x, y: y + length)
+        case .south: return Coordinate(x: x, y: y - length)
+        case .west: return Coordinate(x: x - length, y: y)
+        case .east: return Coordinate(x: x + length, y: y)
+        }
+    }
+}
+
+//: Possible orientations
+public enum Orientation {
+    case north, south, east, west
+    //: Rotates the orientation
+    public func rotate(turn: Turn) -> Orientation {
+        switch self {
+        case .north:
+            switch turn {
+            case .left:
+                return .west
+            case .right:
+                return .east
+            }
+        case .south:
+            switch turn {
+            case .left:
+                return .east
+            case .right:
+                return .west
+            }
+        case .east:
+            switch turn {
+            case .left:
+                return .north
+            case .right:
+                return .south
+            }
+        case .west:
+            switch turn {
+            case .left:
+                return .south
+            case .right:
+                return .north
+            }
+        }
+    }
+}
+
+//: Describes the move
+public struct Move {
+    public let turn: Turn
+    public let length: Int
+    
+    init(turn: Turn, length: Int) {
+        self.turn = turn
+        self.length = length
+    }
+}
+
+//: Describes the position
+public struct Position {
+    public let coordniate: Coordinate
+    public let orientation: Orientation
+    
+    public init(coordniate: Coordinate, orientation: Orientation) {
+        self.coordniate = coordniate
+        self.orientation = orientation
+    }
+    
+    //: Perform actual move
+    public func walked(_ action: Move) -> Position {
+        let orientation = self.orientation.rotate(turn: action.turn)
+        let coordinate = self.coordniate.moved(length: action.length, in: orientation)
+        return Position(coordniate: coordinate, orientation: orientation)
+    }
+}
+//: ## Helpers
+
+extension Position: CustomStringConvertible {
+    public var description: String {
+        return "\(coordniate) - \(orientation)"
+    }
+}
+
+extension Coordinate: CustomStringConvertible {
+    public var description: String {
+        return "(\(x),\(y))"
+    }
+}
+
+extension Move: CustomStringConvertible {
+    public var description: String {
+        let arrow: String
+        switch turn {
+        case .left:
+            arrow = "⟸"
+        case .right:
+            arrow = "⟹"
+        }
+        return "(\(arrow)\(length))"
+    }
+}
+
+public extension Position {
+    public func walk(_ moves: [Move]) -> Position {
+        return moves.reduce(self) { position, move in
+            return position.walked(move)
+        }
+    }
+}
+
+public extension String {
+    public func moves() -> [Move] {
+        do {
+            return try components(separatedBy: ", ").map { try Move(raw: $0) }
+        } catch let error {
+            print(error)
+            return []
+        }
+    }
+}
+
+
+public extension Move {
+    public init(raw: String) throws {
+        guard raw.characters.count >= 2 else { throw ParsingError.invalidLength }
+        let turnString = String(raw[raw.startIndex])
+        let lengthString = raw.substring(from: raw.index(after: raw.startIndex))
+        guard let parsedLength = Int(lengthString)  else  { throw ParsingError.invalidLength }
+        switch turnString {
+        case "L": turn = .left
+        case "R": turn = .right
+        default: throw ParsingError.invalidTurn
+        }
+        length = parsedLength
+    }
+    enum ParsingError: Error {
+        case invalidString
+        case invalidLength
+        case invalidTurn
+    }
+}
+
